1afec3ce3a29332cafe1eaa4fc3cef2b38972692
joyang1 <tingzai.yang@gmail.com>
2017-11-29 14:05:58 +0800

    修改sql脚本

--- sql/erweimastore.sql
+++ sql/erweimastore.sql
@@ -9,7 +9,7 @@ CREATE TABLE `admins` (
   `type` varchar(10)  NOT NULL DEFAULT '0' COMMENT '管理员等级',
   `status` int(11) NOT NULL DEFAULT 1 COMMENT '状态 0-删除 1-正常',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COMMENT '管理员表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '管理员表';
 
 INSERT INTO admins (id, name, nickname, passwd, sex) VALUES (1, 'admin', 'testadmin', '123456', '0');
 
@@ -20,7 +20,7 @@ CREATE TABLE `stores` (
   `qrCodePath` varchar(100) NOT NULL DEFAULT '' COMMENT '店铺二维码路径',
   PRIMARY KEY (`id`),
   UNIQUE (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COMMENT '店铺表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '店铺表';
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,4 +32,4 @@ CREATE TABLE `products` (
   `storeName` varchar(100) NOT NULL COMMENT '所属店铺',
   PRIMARY KEY (`id`),
   UNIQUE (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COMMENT '商品表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '商品表';
